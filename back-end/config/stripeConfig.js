e763a8a1b3cc335311706fddca9d15d2e27ecfa0
vaibhavapriya <vaibhavapriyandv@gmail.com>
2025-01-05 22:19:45 +0530

    env

--- back-end/config/stripeConfig.js
+++ back-end/config/stripeConfig.js
@@ -1,3 +1,3 @@
 const Stripe = require('stripe');
-const stripe = new Stripe("sk_test_51QdwPyRp7phkNDrXEKCj4GqHvAXXy3yAZEIv7tM4bRBs3CC0SP2wqClmtwx02Db7adujKpPUMI8mw0o1PsLNynVt00AoWATpgv"); // Replace with your secret key
+const stripe = new Stripe(process.env.STRIPE_SK); 
 module.exports = stripe;

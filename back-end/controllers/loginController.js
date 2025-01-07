9ddcafef7f1a15a3f13b546f7aab0de44d73a4dc
vaibhavapriya <vaibhavapriyandv@gmail.com>
2025-01-07 09:57:31 +0530

    resetpass

--- back-end/controllers/loginController.js
+++ back-end/controllers/loginController.js
@@ -16,7 +16,7 @@ exports.forgotPassword = async (req, res, next) => {
     user.resetTokenExpiry = resetTokenExpiry;
     await user.save();
     
-    const resetLink = `https://loquacious-florentine-32cf70.netlify.app/${resetToken}`;
+    const resetLink = `https://loquacious-florentine-32cf70.netlify.app/reset-password/${resetToken}`;
 
     // Send email
     const transporter = nodemailer.createTransport({
